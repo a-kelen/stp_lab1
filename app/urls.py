330f3f61c052c72b1a961068bcf2b9ebb72ddd5d
kalancha.artem <kalancha.artem.d@chnu.edu.ua>
2022-04-17 19:33:31 +0300

    Commit lab_5

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path(r'articles', ArticleList.as_view(), name='articles-list'),
     path(r'articles/category/<slug>', ArticleCategoryList.as_view(), name='articles-category-list'),
     path(r'articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name='news-detail')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
